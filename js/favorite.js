de286e05f1adc2fdc9d4fa4a65397afaab633b53
Thanatat Srisung <thanatat.sr@gmail.com>
2024-09-24 23:36:10 +0700

    Favorite page function.

--- js/favorite.js
+++ js/favorite.js
@@ -1,4 +1,4 @@
-
+import shoesData from "../data/shoes-data.js";
 
 const getFavorites = () => {
   return JSON.parse(localStorage.getItem("favorites")) || [];
@@ -35,5 +35,55 @@ const updateCartButtonState = () => {
   }
 };
 
+const createFavoriteItem = () => {
+  const favorites = getFavorites();
+  const favoriteContainerHeader = document.getElementById(
+    "favorite-container-header"
+  );
+  const favoriteContainerContent = document.getElementById(
+    "favorite-container-content"
+  );
+  const container = document.getElementById("container");
+
+  if (!favorites || favorites.length <= 0) {
+    favoriteContainerHeader.innerHTML = `<h4>My Favorite</h4>
+          <p>0 ITEM</p>
+          <p>
+            You haven't saved any items to your wishlist yet. Start shopping and
+            add your favorite items to your wishlist.
+          </p>`;
+  }
+  favorites.forEach((favorite) => {
+    const productId = favorite.productId.substring(0, 2);
+    const colorVariantIndex = favorite.productId.substring(2);
+    let shoe = shoesData.find((sh) => sh.id === productId);
+    let colorVariant = shoe.colorVariants.find(
+      (cv) => cv.index === colorVariantIndex
+    );
+    if (favorites.length > 4) {
+      container.style.height = "auto";
+    }
+    favoriteContainerHeader.innerHTML = `<h4>My Favorite</h4>
+          <p>${favorites.length} ${
+      favorites.length > 1 ? "ITEMS" : "ITEM"
+    }</p>`;
+    const favoriteContent = document.createElement("a");
+    favoriteContent.href = `product.html?id=${favorite.productId}`;
+    favoriteContent.ariaLabel = `${shoe.name}`;
+    favoriteContent.innerHTML = `<div class="product-card promotion bestseller">
+              <img
+                src="${colorVariant.previewImages[0]}"
+                alt="${shoe.name}"
+              />
+              <div class="product-content">
+                <p class="product-title">${shoe.name}</p>
+                <p class="product-price">£119.99</p>
+              </div>
+            </div>`;
+    favoriteContainerContent.appendChild(favoriteContent);
+  });
+};
+
 updateFavButtonState();
 updateCartButtonState();
+createFavoriteItem();
